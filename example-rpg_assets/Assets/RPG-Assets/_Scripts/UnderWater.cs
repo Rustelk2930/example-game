75b8c56dc1d65f9ec8146a0cd7d1235d30fff038
Rustelk2930 <cswobodatb@gmail.com>
2021-04-06 14:08:18 -0500

    Add project

---
+++ example-rpg_assets/Assets/RPG-Assets/_Scripts/UnderWater.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderWater : MonoBehaviour
+{
+    [SerializeField] float seeInWaterDistance = .1f;
+    [SerializeField] Material skybox = null;
+    [SerializeField] Material underWaterSkybox = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {            
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = new Vector4(0.2f, 0.4f, 0.4f);
+            RenderSettings.fogDensity = seeInWaterDistance;
+            RenderSettings.skybox = underWaterSkybox;
+        }
+        if(other.tag == "Head")
+        {
+            other.gameObject.GetComponentInParent<PlayerMotor>().HeadAboveWaterCheck(false);
+        }
+        if (other.tag == "Body")
+        {
+            other.gameObject.GetComponentInParent<PlayerController>().ChangeSwimmingMode(true);
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {            
+            RenderSettings.fog = false;
+            RenderSettings.skybox = skybox;
+        }
+        if(other.tag == "Head")
+        {
+            other.gameObject.GetComponentInParent<PlayerMotor>().HeadAboveWaterCheck(true);
+        }
+        if (other.tag == "Body")
+        {
+            other.gameObject.GetComponentInParent<PlayerController>().ChangeSwimmingMode(false);
+        }
+    }
+}
